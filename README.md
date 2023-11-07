413e3ba766c21edaac2d414149b7a08e841e2ea8
Jarosław Michna <jaroslaw.michna@gmail.com>
2023-11-07 21:43:45 +0100

    Aplga 1

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Kożuchy Paprocka
 <h2>Kożuchy Paprocka - projekt prywatny</h2>
-<p>Date: Oct 2023 r. (Last update: -)</p>
+<p>Date: Oct 2023 r. (Last update: 07.11.2023 r.)</p>
 <p><strong>Used</strong>: Bootstrap 5, Sass, FontAwesome, jQuery, Freebie gallery, baguetteBox.</p>
 <p><strong>Working with</strong>: Visual Studio Code, Grunt, Eslint.</p>
 <h3>Changelog:</h3>
 <ul>
-<li>-</li>
+  <li>2023.11.07 - Alpha 1</li>
 </ul>
--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 $(function() {
-  // Smooth scrolling main menu
+  //CHANGING LIST OF CLASS WHEN MENU TOGGLE BUTTON CLICKED
+  let menuElement = document.getElementById('mainmenu-ScrollSpy');
+  let toggler = document.getElementById('menu-toggler-button');
+  let logo = document.getElementById('logo');
+  $(toggler).click(function(event) {
+    var togglerState = toggler.getAttribute('aria-expanded');
+    if (togglerState == 'true') {
+      menuElement.classList.add('expanded-Menu');
+      logo.classList.add('expanded-Logo');
+    } else {
+      menuElement.classList.remove('expanded-Menu');
+      logo.classList.remove('expanded-Logo');
+    }
+  });
+  $('button.navbar-toggler.collapsed').click(function(event) {
+    menuElement.classList.add('collapsed-Menu');
+  });
+
+  // Smooth scrolling
   $('a.smoothScroll').click(function(event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== '') {
@@ -15,8 +33,16 @@ $(function() {
     }
   });
 
-  // TO-TOP SHOW AND HIDE
+  // TO-TOP SHOW AND HIDE WHEN SCROLL
+  // LOGO AND MENU LIST OF CLASS MODIFY WHEN SCROLL
   $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      menuElement.classList.add('fixed-Menu');
+      logo.classList.add('fixed-Logo');
+    } else {
+      menuElement.classList.remove('fixed-Menu');
+      logo.classList.remove('fixed-Logo');
+    }
 		if ($(this).scrollTop() > 250) {
 			$('#totop').fadeIn('slow');
 		} else {
@@ -27,10 +53,15 @@ $(function() {
   // Copyrights year fill
   $(window).ready(function() {
     let year = new Date().getFullYear();
-    let start = $('#start-year').html();
-    if (parseInt(year) > parseInt(start))
-      $('#this-year').html(` - ${year}`);
+    $('#this-year').html(`${year}`);
     return;
   });
 
+  //Bootstrap Scrollspy refresh when filtr option change
+  $('input.opcja-Filtr').click(function() {
+    const dataSpyList = document.querySelectorAll('[data-bs-spy="scroll"]');
+    dataSpyList.forEach(dataSpyEl => {
+      bootstrap.ScrollSpy.getInstance(dataSpyEl).refresh();
+    });
+  });
 });
